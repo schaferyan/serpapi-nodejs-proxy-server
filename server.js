40fb9e2de0a29b3ff2f1244579ee50d233218856
Ryan Schafer <schaferyan@gmail.com>
2023-03-27 10:34:55 -0700

    configured cors

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ import dotenv from "dotenv";
 import { config} from "serpapi";
 import cors from "cors";
 import {keywordSearch, getResults, postResults} from './search.js';
-// import router from "./routes.js";
 
 
-dotenv.config();
+
+dotenv.config(); 
 // console.log(process.env);
 config.api_key = process.env.SERPAPI_KEY;
 
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: "http://yourdomain.com"}));
 app.use(json());
 
 const PORT = process.env.PORT || 3000;
@@ -21,17 +21,6 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "hello world!" });
   });
 
-// app.get("/search", async (req, res) => {
-//   const params = {...req.query};
-//   try {
-//     const data = await getJson("google", params);
-//     res.locals.result = data;
-//     res.status(200);
-//   } catch (error) {
-//     console.log(error)
-//     res.status(400);
-//   }
-// })
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "default get request" });
@@ -48,9 +37,6 @@ app.get("/search", getResults, (req, res) => {
 app.post("/search", postResults, (req, res) => {
     res.status(200).json(res.locals.result);
 });
-
-
-// app.use("/api/", router);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}\n-------------------------`);
